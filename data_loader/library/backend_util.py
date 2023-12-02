2ae5e127dd5f6a4945dc59558db4af8010292768
Kamburugamuwa, Pasan <pkamburu@iu.edu>
2023-12-01 23:40:50 -0500

    add dataloader

---
+++ data_loader/library/backend_util.py
@@ -0,0 +1,191 @@
+"""
+Purpose:
+    This script used to read the data from the project.
+    and pass these data to other modules in backend.
+Inputs:
+    - No inputs to this file
+Outputs:
+    The project configurations will be passed.
+Author: Pasan Kamburugamuwa
+"""
+
+import configparser
+import logging
+import traceback
+import os
+import sys
+from psycopg2 import pool
+from contextlib import contextmanager
+
+LOG_DIR = "/Users/pkamburu/IUNI/ECC-Project/log"
+LOG_FNAME = "database_server.log"
+
+def get_mastodon_conf():
+    """
+    Get the mastodon configuration file.
+    """
+    try:
+        config_file_path = "./config/project.config"
+        config_parser = configparser.ConfigParser()
+        config_parser.read(config_file_path)
+        return config_parser
+    except FileNotFoundError as fnf_error:
+        traceback.print_tb(fnf_error.__traceback__)
+        raise Exception('Unable to find the mastodon config file')
+
+def get_flask_host():
+    """
+    Get the flask host.
+    """
+    try:
+        config = get_mastodon_conf()
+        flask_host = config["DEFAULT"]["FlaskHost"]
+        return flask_host
+    except Exception as exc:
+        traceback.print_tb(exc.__traceback__)
+        raise Exception('Unable to find the mastodon flask host')
+
+def get_flask_port():
+    """
+    Get the flask port.
+    """
+    try:
+        config = get_mastodon_conf()
+        flask_port = config["DEFAULT"]["FlaskPort"]
+        return flask_port
+    except Exception as exc:
+        traceback.print_tb(exc.__traceback__)
+        raise Exception('Unable to find the mastodon flask port')
+
+def get_flask_debug_mode():
+    """
+    Get the flask debug mode.
+    """
+    try:
+        config = get_mastodon_conf()
+        flask_debug_mode = config["DEFAULT"]["DebugMode"]
+        return flask_debug_mode
+    except Exception as exc:
+        traceback.print_tb(exc.__traceback__)
+        raise Exception('Unable to find the mastodon debug mode')
+
+def get_fib_index_conf():
+    try:
+        config_parser = configparser.ConfigParser()
+        config_parser.read(get_mastodon_conf)
+        return config_parser
+    except FileNotFoundError as fnf_error:
+        traceback.print_tb(fnf_error.__traceback__)
+        raise Exception('Unable to find the fibIndex config file')
+
+def get_database_host():
+    try:
+        config = get_fib_index_conf()
+        database_host = 'localhost'
+        return database_host
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        raise Exception('Unable to find the postgresql database host')
+
+def get_database_port():
+    try:
+        config = get_fib_index_conf()
+        database_port = config["POSTGRESQL_DATABASE"]["database-port"]
+        return database_port
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        raise Exception('Unable to find the postgresql database port')
+
+def get_database_name():
+    try:
+        config = get_fib_index_conf()
+        database_name = config["POSTGRESQL_DATABASE"]["database-name"]
+        return database_name
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        raise Exception('Unable to find the postgresql database name')
+
+def get_database_username():
+    try:
+        config = get_fib_index_conf()
+        database_username = config["POSTGRESQL_DATABASE"]["database-username"]
+        return database_username
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        raise Exception('Unable to find the postgresql database username')
+
+def get_database_password():
+    try:
+        config = get_fib_index_conf()
+        database_password = config["POSTGRESQL_DATABASE"]["database-password"]
+        return database_password
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        raise Exception('Unable to find the postgresql database password')
+
+db = None
+
+try:
+    db = pool.SimpleConnectionPool(1,
+                                10,
+                                host=get_database_host(),
+                                database=get_database_name(),
+                                user=get_database_username(),
+                                password=get_database_password(),
+                                port=get_database_port())
+except:
+    pass
+
+@contextmanager
+def get_db_connection():
+    con = db.getconn()
+    try:
+        yield con
+    finally:
+        db.putconn(con)
+
+
+@contextmanager
+def get_db_cursor():
+    with get_db_connection() as connection:
+        cursor = connection.cursor()
+        try:
+            yield cursor
+            connection.commit()
+        finally:
+            cursor.close()
+
+def get_logger(LOG_DIR, LOG_FNAME, script_name=None, also_print=False):
+    """
+    Create logger for the project.
+    """
+    # Create log_dir if it doesn't exist already
+    try:
+        os.makedirs(f"{LOG_DIR}")
+    except:
+        pass
+
+    # Create logger and set level
+    logger = logging.getLogger(script_name)
+    logger.setLevel(level=logging.INFO)
+
+    # Configure file handler
+    formatter = logging.Formatter(
+        fmt="%(asctime)s-%(name)s-%(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d_%H:%M:%S",
+    )
+    full_log_path = os.path.join(LOG_DIR, LOG_FNAME)
+    fh = logging.FileHandler(f"{full_log_path}")
+    fh.setFormatter(formatter)
+    fh.setLevel(level=logging.INFO)
+    # Add handlers to logger
+    logger.addHandler(fh)
+
+    # If also_print is true, the logger will also print the output to the
+    # console in addition to sending it to the log file
+    if also_print:
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(formatter)
+        ch.setLevel(level=logging.INFO)
+        logger.addHandler(ch)
+    return logger
--- data_loader/server.py
+++ data_loader/server.py
@@ -1,39 +1,41 @@
-import psycopg2
+"""
+Purpose:
+    This script used to insert items into the table
 
-# Connection parameters
-connection_params = {
-    "user": "postgres",
-    "password": "test1234",
-    "host": "192.168.225.86",
-    "port": "5432",
-    "database": "postgres"
-}
+Outputs:
+    - JSON object
+Authors: Pasan Kamburugamuwa
+"""
+from flask import Flask
+from flask_cors import CORS
+from library import backend_util
+import os
+import pandas as pd
+from database_functions import breast_cancer
 
-def connect_db():
-    try:
-        # Connect to PostgreSQL
-        print("Connecting to PostgreSQL ...")
-        connection = psycopg2.connect(**connection_params)
 
-        # Create a cursor object
-        cursor = connection.cursor()
+LOG_DIR = "/Users/pkamburu/IUNI/ECC-Project/log"
+LOG_FNAME = "database_server.log"
 
-        # Execute a simple query
-        cursor.execute("SELECT now() AS run_at;")
-        result = cursor.fetchone()
+app = Flask(__name__)
+CORS(app)
 
-        # Print the result
-        print(f"Run at date-time: {result[0]}")
+def read_csv_file_process():
 
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
+    file_path = '/Users/pkamburu/IUNI/ECC-Project/data/data.csv'
+    df = pd.read_csv(file_path)
 
-        print("Execution Completed ...")
+    for index, row in df.iterrows():
+        breast_cancer.add_data(row['id'], row['diagnosis'], row['radius_mean'], row['texture_mean'], row['perimeter_mean'], row['area_mean'], row['smoothness_mean'], row['compactness_mean'], row['concavity_mean'], row['concave points_mean'], row['symmetry_mean'],
+                               row['fractal_dimension_mean'], row['radius_se'], row['texture_se'], row['perimeter_se'], row['area_se'], row['smoothness_se'],row['compactness_se'], row['concavity_se'], row['concave points_se'], row['symmetry_se'], row['radius_worst'], row['texture_worst'],
+                               row['perimeter_worst'], row['area_worst'], row['smoothness_worst'] ,row['compactness_worst'] ,row['concavity_worst'], row['concave points_worst'], row['symmetry_worst'], row['fractal_dimension_worst'])
 
-    except Exception as e:
-        print(f"Error while connecting to the database: {e}")
+if __name__ == '__main__':
+    script_name = os.path.basename(__file__)
+    logger = backend_util.get_logger(LOG_DIR, LOG_FNAME, script_name=script_name, also_print=True)
+    logger.info("-" * 50)
+    logger.info(f"Begin script: {__file__}")
 
-# Call the function to connect to the database
-connect_db()
+    read_csv_file_process()
 
+    app.run(host=backend_util.get_flask_host(), port=int(backend_util.get_flask_port()), debug=backend_util.get_flask_debug_mode())
