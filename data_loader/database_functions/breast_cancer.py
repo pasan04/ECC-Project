1077b0b7451395f80b1b7dbc675afad7a9d93e12
Kamburugamuwa, Pasan <pkamburu@iu.edu>
2023-12-02 00:41:44 -0500

    add the data loader function

--- data_loader/database_functions/breast_cancer.py
+++ data_loader/database_functions/breast_cancer.py
@@ -1,110 +1,40 @@
-"""
-Purpose:
-    Contains functions used to add data to breast_cancer table
-
-Inputs:
-    None
-
-Outputs:
-    None
-
-Authors: Pasan Kamburugamuwa & Matthew DeVerna
-"""
-from flask import Flask
 from library import backend_util
+import psycopg2
 
-app = Flask(__name__)
-
-def add_data(id, diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean,compactness_mean,
-             concavity_mean, concave_points_mean, symmetry_mean,fractal_dimension_mean, radius_se, texture_se, perimeter_se,
-             area_se,smoothness_se, compactness_se, concavity_se,concave_points_se,symmetry_se, radius_worst,  texture_worst, perimeter_worst,
-             area_worst,smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst ):
-    """
-    Add data to the breast_cancer database table.
-
-    Parameters
-    -----------
-        id int NOT NULL,
-        diagnosis char(10) NOT NULL,
-        radius_mean float,
-        texture_mean float,
-        perimeter_mean float,
-        area_mean float,
-        smoothness_mean float,
-        compactness_mean float,
-        concavity_mean float,
-        concave_points_mean float,
-        symmetry_mean float,
-        fractal_dimension_mean float,
-        radius_se float,
-        texture_se float,
-        perimeter_se float,
-        area_se float,
-        smoothness_se float,
-        compactness_se float,
-        concavity_se float,
-        concave_points_se float,
-        symmetry_se float,
-        radius_worst float,
-        texture_worst float,
-        perimeter_worst float,
-        area_worst float,
-        smoothness_worst float,
-        compactness_worst float,
-        concavity_worst float,
-        concave_points_worst float,
-        symmetry_worst float,
-        fractal_dimension_worst float,
-    Returns
-    -----------
-    result (dict): {'id': record_id}
-    """
-    with backend_util.get_db_cursor() as cur:
-        try:
-            # Table creation SQL statement
-            table_creation_query = """
-            CREATE TABLE breast_cancer_data (
-                id SERIAL PRIMARY KEY,
-                diagnosis CHAR(10) NOT NULL,
-                radius_mean FLOAT,
-                texture_mean FLOAT,
-                perimeter_mean FLOAT,
-                area_mean FLOAT,
-                smoothness_mean FLOAT,
-                compactness_mean FLOAT,
-                concavity_mean FLOAT,
-                concave_points_mean FLOAT,
-                symmetry_mean FLOAT,
-                fractal_dimension_mean FLOAT,
-                radius_se FLOAT,
-                texture_se FLOAT,
-                perimeter_se FLOAT,
-                area_se FLOAT,
-                smoothness_se FLOAT,
-                compactness_se FLOAT,
-                concavity_se FLOAT,
-                concave_points_se FLOAT,
-                symmetry_se FLOAT,
-                radius_worst FLOAT,
-                texture_worst FLOAT,
-                perimeter_worst FLOAT,
-                area_worst FLOAT,
-                smoothness_worst FLOAT,
-                compactness_worst FLOAT,
-                concavity_worst FLOAT,
-                concave_points_worst FLOAT,
-                symmetry_worst FLOAT,
-                fractal_dimension_worst FLOAT
-            );
+def add_data(id, diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean,
+             concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se,
+             perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se,
+             radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst,
+             concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst):
+    try:
+        with backend_util.get_db_cursor() as cur:
+            # Data insertion SQL statement
+            data_insertion_query = """
+            INSERT INTO breast_cancer_data (
+                id, diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean,
+                concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se,
+                perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se,
+                radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst,
+                concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst
+            ) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            ) RETURNING id;
             """
 
-            cur.execute(table_creation_query, (id, diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean,compactness_mean,
-             concavity_mean, concave_points_mean, symmetry_mean,fractal_dimension_mean, radius_se, texture_se, perimeter_se,
-             area_se,smoothness_se, compactness_se, concavity_se,concave_points_se,symmetry_se, radius_worst,  texture_worst, perimeter_worst,
-             area_worst,smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst ))
+            cur.execute(data_insertion_query, (id, diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean,
+                                               smoothness_mean, compactness_mean, concavity_mean, concave_points_mean,
+                                               symmetry_mean, fractal_dimension_mean, radius_se, texture_se,
+                                               perimeter_se, area_se, smoothness_se, compactness_se, concavity_se,
+                                               concave_points_se, symmetry_se, radius_worst, texture_worst,
+                                               perimeter_worst, area_worst, smoothness_worst, compactness_worst,
+                                               concavity_worst, concave_points_worst, symmetry_worst,
+                                               fractal_dimension_worst))
+
+            result = {}
             if cur.rowcount > 0:
-                id = cur.fetchone()[0]
-                result = {"record_id": id}
+                record_id = cur.fetchone()[0]
+                result = {"record_id": record_id}
             return result
-        except Exception as ex:
-            raise Exception(ex)
+    except psycopg2.Error as ex:
+        raise Exception(f"Error while adding data: {ex}")
--- data_loader/library/backend_util.py
+++ data_loader/library/backend_util.py
@@ -20,7 +20,7 @@ from contextlib import contextmanager
 LOG_DIR = "/Users/pkamburu/IUNI/ECC-Project/log"
 LOG_FNAME = "database_server.log"
 
-def get_mastodon_conf():
+def get_project_conf():
     """
     Get the mastodon configuration file.
     """
@@ -33,63 +33,21 @@ def get_mastodon_conf():
         traceback.print_tb(fnf_error.__traceback__)
         raise Exception('Unable to find the mastodon config file')
 
-def get_flask_host():
-    """
-    Get the flask host.
-    """
-    try:
-        config = get_mastodon_conf()
-        flask_host = config["DEFAULT"]["FlaskHost"]
-        return flask_host
-    except Exception as exc:
-        traceback.print_tb(exc.__traceback__)
-        raise Exception('Unable to find the mastodon flask host')
-
-def get_flask_port():
-    """
-    Get the flask port.
-    """
-    try:
-        config = get_mastodon_conf()
-        flask_port = config["DEFAULT"]["FlaskPort"]
-        return flask_port
-    except Exception as exc:
-        traceback.print_tb(exc.__traceback__)
-        raise Exception('Unable to find the mastodon flask port')
-
-def get_flask_debug_mode():
-    """
-    Get the flask debug mode.
-    """
-    try:
-        config = get_mastodon_conf()
-        flask_debug_mode = config["DEFAULT"]["DebugMode"]
-        return flask_debug_mode
-    except Exception as exc:
-        traceback.print_tb(exc.__traceback__)
-        raise Exception('Unable to find the mastodon debug mode')
-
-def get_fib_index_conf():
-    try:
-        config_parser = configparser.ConfigParser()
-        config_parser.read(get_mastodon_conf)
-        return config_parser
-    except FileNotFoundError as fnf_error:
-        traceback.print_tb(fnf_error.__traceback__)
-        raise Exception('Unable to find the fibIndex config file')
-
 def get_database_host():
+    """
+    Get the database host.
+    """
     try:
-        config = get_fib_index_conf()
-        database_host = 'localhost'
+        config = get_project_conf()
+        database_host = config["POSTGRESQL_DATABASE"]["database-host"]
         return database_host
-    except Exception as e:
-        traceback.print_tb(e.__traceback__)
-        raise Exception('Unable to find the postgresql database host')
+    except Exception as exc:
+        traceback.print_tb(exc.__traceback__)
+        raise Exception('Unable to find the database host')
 
 def get_database_port():
     try:
-        config = get_fib_index_conf()
+        config = get_project_conf()
         database_port = config["POSTGRESQL_DATABASE"]["database-port"]
         return database_port
     except Exception as e:
@@ -98,7 +56,7 @@ def get_database_port():
 
 def get_database_name():
     try:
-        config = get_fib_index_conf()
+        config = get_project_conf()
         database_name = config["POSTGRESQL_DATABASE"]["database-name"]
         return database_name
     except Exception as e:
@@ -107,7 +65,7 @@ def get_database_name():
 
 def get_database_username():
     try:
-        config = get_fib_index_conf()
+        config = get_project_conf()
         database_username = config["POSTGRESQL_DATABASE"]["database-username"]
         return database_username
     except Exception as e:
@@ -116,7 +74,7 @@ def get_database_username():
 
 def get_database_password():
     try:
-        config = get_fib_index_conf()
+        config = get_project_conf()
         database_password = config["POSTGRESQL_DATABASE"]["database-password"]
         return database_password
     except Exception as e:
@@ -124,6 +82,7 @@ def get_database_password():
         raise Exception('Unable to find the postgresql database password')
 
 db = None
+
 
 try:
     db = pool.SimpleConnectionPool(1,
